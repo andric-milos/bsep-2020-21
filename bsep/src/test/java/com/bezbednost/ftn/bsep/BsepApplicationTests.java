def663fa3193b14792fb668a9f3fc3ee8392f81a
meelosch <57445151+meelosch@users.noreply.github.com>
2021-03-16 18:30:17 +0100

    initial project commit

---
+++ bsep/src/test/java/com/bezbednost/ftn/bsep/BsepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezbednost.ftn.bsep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
