3519c048c9c0c433cb59c8a03523228f29c4fc1d
Nikola-Kekic <nikola.get@gmail.com>
2021-03-21 21:43:03 +0100

    Database connection

--- bsep/src/main/java/com/bezbednost/ftn/bsep/model/IssuerAndSubjectData.java
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/model/IssuerAndSubjectData.java
@@ -3,55 +3,91 @@ package com.bezbednost.ftn.bsep.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
+
+@Entity
 public class IssuerAndSubjectData {
 
-    // private Long id;
-    private String firstNameIssuer;
-    private String lastNameIssuer;
-    private String organizationIssuer;
-    // private String organizationUnitIssuer;
-    private String countryIssuer;
-    private String cityIssuer;
-    private String emailIssuer;
-    // private String phoneIssuer;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column
+    private String firstNameIssuer;
+
+    @Column
+    private String lastNameIssuer;
+
+    @Column
+    private String organizationIssuer;
+
+    @Column
+    private String countryIssuer;
+
+    @Column
+    private String cityIssuer;
+
+    @Column
+    private String emailIssuer;
+
+    @Enumerated
+    private TypeOfEntity typeOfEntity;
+
+    @Enumerated(value = EnumType.STRING)
     private CertificateRole certificateRole;
 
+    // @Transient anotacija sprecava cuvanje podataka u mysql
+    @Transient
     private String firstNameSubject;
+
+    @Transient
     private String lastNameSubject;
+
+    @Transient
     private String organizationSubject;
-    // private String organizationUnitSubject;
+
+    @Transient
     private String countrySubject;
+
+    @Transient
     private String citySubject;
+
+    @Transient
     private String emailSubject;
-    // private String phoneSubject;
 
-    // private CertificateStatus certificateStatus;
+    @Column
+    private boolean[] keyUsage;
 
-    // private Long parent;
+    @Column
+    private boolean[] extendedKeyUsage;
 
+    @Enumerated(value = EnumType.STRING)
+    private CertificateStatus certificateStatus;
+
+    @Column
+    private Long parentId;
+
+    @Column
     Date startDate;
+
+    @Column
     Date expiringDate;
 
-    public IssuerAndSubjectData(String firstNameIssuer,
-                                String lastNameIssuer,
-                                String organizationIssuer,
-                                String countryIssuer,
-                                String cityIssuer,
-                                String emailIssuer,
-                                CertificateRole certificateRole,
-                                String firstNameSubject,
-                                String lastNameSubject,
-                                String organizationSubject,
-                                String countrySubject,
-                                String citySubject,
-                                String emailSubject,
-                                Date startDate,
-                                Date expiringDate) {
+    public IssuerAndSubjectData() {
+    }
+
+    public IssuerAndSubjectData(String firstNameIssuer, String lastNameIssuer, String organizationIssuer,
+                                String countryIssuer, String cityIssuer, String emailIssuer,
+                                CertificateRole certificateRole, String firstNameSubject,
+                                String lastNameSubject, String organizationSubject, String countrySubject,
+                                String citySubject, String emailSubject, CertificateStatus certificateStatus,
+                                Date startDate, Date expiringDate, boolean[] keyUsage,boolean[] extendedKeyUsage) {
         this.firstNameIssuer = firstNameIssuer;
         this.lastNameIssuer = lastNameIssuer;
         this.organizationIssuer = organizationIssuer;
@@ -65,7 +101,55 @@ public class IssuerAndSubjectData {
         this.countrySubject = countrySubject;
         this.citySubject = citySubject;
         this.emailSubject = emailSubject;
+        this.certificateStatus = certificateStatus;
+        this.startDate = startDate;
+        this.expiringDate = expiringDate;
+        this.keyUsage = keyUsage;
+        this.extendedKeyUsage = extendedKeyUsage;
+    }
+
+    public IssuerAndSubjectData(String firstName, String lastName, String organization, String country,
+                                String city, String email, TypeOfEntity typeOfEntity,
+                                CertificateRole certificateRole,boolean[] keyUsage,boolean[] extendedKeyUsage) {
+        this.firstNameIssuer = firstName;
+        this.lastNameIssuer = lastName;
+        this.organizationIssuer = organization;
+        this.countryIssuer = country;
+        this.cityIssuer = city;
+        this.emailIssuer = email;
+        this.typeOfEntity = typeOfEntity;
+        this.certificateRole = certificateRole;
+        this.certificateStatus = CertificateStatus.VALID;
+        this.keyUsage = keyUsage;
+        this.extendedKeyUsage = extendedKeyUsage;
+    }
+
+    public IssuerAndSubjectData(Long id, String firstNameIssuer, String lastNameIssuer, String organizationIssuer,
+                                String countryIssuer, String cityIssuer, String emailIssuer, TypeOfEntity typeOfEntity,
+                                CertificateRole certificateRole, String firstNameSubject, String lastNameSubject, String organizationSubject,
+                                String countrySubject, String citySubject, String emailSubject, boolean[] keyUsage, boolean[] extendedKeyUsage,
+                                CertificateStatus certificateStatus, Long parentId, Date startDate, Date expiringDate) {
+        this.id = id;
+        this.firstNameIssuer = firstNameIssuer;
+        this.lastNameIssuer = lastNameIssuer;
+        this.organizationIssuer = organizationIssuer;
+        this.countryIssuer = countryIssuer;
+        this.cityIssuer = cityIssuer;
+        this.emailIssuer = emailIssuer;
+        this.typeOfEntity = typeOfEntity;
+        this.certificateRole = certificateRole;
+        this.firstNameSubject = firstNameSubject;
+        this.lastNameSubject = lastNameSubject;
+        this.organizationSubject = organizationSubject;
+        this.countrySubject = countrySubject;
+        this.citySubject = citySubject;
+        this.emailSubject = emailSubject;
+        this.keyUsage = keyUsage;
+        this.extendedKeyUsage = extendedKeyUsage;
+        this.certificateStatus = certificateStatus;
+        this.parentId = parentId;
         this.startDate = startDate;
         this.expiringDate = expiringDate;
     }
+
 }
---
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/repository/IssuerAndSubjectDataRepository.java
@@ -0,0 +1,22 @@
+package com.bezbednost.ftn.bsep.repository;
+
+import com.bezbednost.ftn.bsep.model.IssuerAndSubjectData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface IssuerAndSubjectDataRepository  extends JpaRepository<IssuerAndSubjectData, Long> {
+
+    @Query(value = "SELECT * FROM issuer_and_subject_data dataa WHERE dataa.email_issuer = :email", nativeQuery = true)
+    IssuerAndSubjectData findByEmail(String email);
+
+    @Query(value = "SELECT * FROM issuer_and_subject_data dataa WHERE dataa.certificate_role = 'SELF_SIGNED' OR dataa.certificate_role = 'INTERMEDIATE' AND dataa.certificate_status = 'VALID'", nativeQuery = true)
+    Collection<IssuerAndSubjectData> getSSAndCA();
+
+    IssuerAndSubjectData findTopByOrderByIdDesc();
+
+}
+
