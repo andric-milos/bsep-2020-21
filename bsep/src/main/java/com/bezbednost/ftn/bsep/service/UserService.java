b0c6906f89b95c4f9ffe4c9de6168658b11181c8
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-04-19 23:41:03 +0200

    created UserRepository and UserService classes

---
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/service/UserService.java
@@ -0,0 +1,26 @@
+package com.bezbednost.ftn.bsep.service;
+
+import com.bezbednost.ftn.bsep.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User with " + email + " not found!")
+        );
+    }
+}
