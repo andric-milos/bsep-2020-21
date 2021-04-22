358793a1995f1f9ad73fecb2de6c3e65da90a7d7
Nikola-Kekic <nikola.get@gmail.com>
2021-04-22 23:57:09 +0200

    login page and jwt implementation through security

--- bsep-frontend/src/views/Login.vue
+++ bsep-frontend/src/views/Login.vue
@@ -2,19 +2,19 @@
   <div class="home">
     <admin-navbar></admin-navbar>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome" />
+    <LogIn msg="Log In" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LogIn from "@/components/LogIn.vue";
 import AdminNavbar from "../components/navbars/AdminNavbar.vue"
 
 export default {
-  name: "Home",
+  name: "Login",
   components: {
-    HelloWorld,
+    LogIn,
     AdminNavbar
   }
 };
