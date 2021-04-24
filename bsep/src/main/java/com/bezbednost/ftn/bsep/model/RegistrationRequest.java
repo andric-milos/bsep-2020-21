d050c73a1215e44f27a460da887082af06ae033f
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-04-25 00:57:07 +0200

    Resolved InvalidDefinitionException
    Jackson couldn't find a way to instantiate RegistrationRequest, because there was no default constructor.

--- bsep/src/main/java/com/bezbednost/ftn/bsep/model/RegistrationRequest.java
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/model/RegistrationRequest.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@NoArgsConstructor
 public class RegistrationRequest {
     private String firstName;
     private String lastName;
