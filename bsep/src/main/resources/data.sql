efe0b4952faee15b7e37dbb9d7f711281ebbc044
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-05-17 23:58:19 +0200

    modified data.sql script

--- bsep/src/main/resources/data.sql
+++ bsep/src/main/resources/data.sql
@@ -11,13 +11,13 @@ null, null, 'VALID', 1, '2020-11-11 13:23:44', '2021-11-11 13:23:44' );
 insert into `bsep`.`authority` (`name`) values ('ADMIN');
 insert into `bsep`.`authority` (`name`) values ('USER');
 
-insert into `bsep`.`users` (`email`, `enabled`, `first_name`, `last_name`, `locked`, `password`, `user_role`)
+insert into `bsep`.`users` (`email`, `enabled`, `first_name`, `last_name`, `locked`, `password`, `user_role`, `city`, `country`)
 values ('nikola@gmail.com', true, 'Nikola', 'Kekic', false,
-'$2y$12$1xiQFNbAsdrLMrxEojq4QuGXbaP74Bam4lOBX7JDBQSfmrgNRMrOK', 'USER');
+'$2y$12$1xiQFNbAsdrLMrxEojq4QuGXbaP74Bam4lOBX7JDBQSfmrgNRMrOK', 'USER', 'Novi Sad', 'Serbia');
 
-insert into `bsep`.`users` (`email`, `enabled`, `first_name`, `last_name`, `locked`, `password`, `user_role`)
+insert into `bsep`.`users` (`email`, `enabled`, `first_name`, `last_name`, `locked`, `password`, `user_role`, `city`, `country`)
 values ('admin@gmail.com', true, 'Admin', 'Admin', false,
-'$2y$12$h/A1KO4/EOdDd6I0G4tEE.KwEWlF5FIqMlGo/f108/qPJvTtW9QGW', 'ADMIN');
+'$2y$12$h/A1KO4/EOdDd6I0G4tEE.KwEWlF5FIqMlGo/f108/qPJvTtW9QGW', 'ADMIN', 'Novi Sad', 'Serbia');
 
 INSERT INTO `bsep`.`user_authority` (`user_id`, `authority_id`) VALUES ('1', '2');
 INSERT INTO `bsep`.`user_authority` (`user_id`, `authority_id`) VALUES ('2', '1');
