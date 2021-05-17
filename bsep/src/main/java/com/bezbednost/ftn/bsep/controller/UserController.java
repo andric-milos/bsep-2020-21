1369a19feb4c85bfe4ec3dee9739c99ebf85b105
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-05-17 23:58:46 +0200

    implemented method for getting all users from database

--- bsep/src/main/java/com/bezbednost/ftn/bsep/controller/UserController.java
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/controller/UserController.java
@@ -4,15 +4,16 @@ import com.bezbednost.ftn.bsep.dto.UserDTO;
 import com.bezbednost.ftn.bsep.model.User;
 import com.bezbednost.ftn.bsep.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -29,6 +30,12 @@ public class UserController {
         UserDetails userDetails = this.userService.loadUserByUsername(user1.getEmail());
         UserDTO retVal = new UserDTO(userDetails);
         return retVal;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        List<User> users = userService.getAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
--- bsep/src/main/java/com/bezbednost/ftn/bsep/service/impl/UserService.java
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/service/impl/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -68,5 +69,9 @@ public class UserService implements UserDetailsService {
 
     public void updateUser(User user) {
         userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
     }
 }
