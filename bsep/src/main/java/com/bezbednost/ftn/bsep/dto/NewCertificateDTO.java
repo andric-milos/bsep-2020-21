4e52caf2bff1779cff30efab1bae0b82e92c2ec1
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-06-01 15:45:31 +0200

    created new DTO class
    this class is used when sending information from client - information that is required for issuing certificate

---
+++ bsep/src/main/java/com/bezbednost/ftn/bsep/dto/NewCertificateDTO.java
@@ -0,0 +1,21 @@
+package com.bezbednost.ftn.bsep.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class NewCertificateDTO {
+    private Long id; // issuer's certificate id (issuerAndSubjectData id)
+    private Long subjectID;
+    private String certificateType;
+    private String keyStorePassword;
+    private Date startDate;
+    private Date endDate;
+}
